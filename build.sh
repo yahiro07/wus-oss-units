a8950df7364393bb95fe15c0905dc5df9714ca71
yahiro <yahiro07@users.noreply.github.com>
2026-05-19 12:46:12 +0900

    add build entry

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ dstDir=./dist
 rm -rf $dstDir
 mkdir -p $dstDir
 cd ./koodori && npm run build && cd ..
+cd ./bc-010 && npm run build && cd ..
 cp -r koodori/dist $dstDir/koodori
+cp -r bc-010/build $dstDir/bc-010
 cp -r additive/web $dstDir/additive
 cp -r drum-machine/web $dstDir/drum-machine
 cp -r wasyn-1/web $dstDir/wasyn-1
